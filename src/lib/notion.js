f18088eb9ca80124022b84802c6cb2575dc820e9
Evren Çağlayan <caglayanevren@gmail.com>
2022-06-08 16:48:16 +0300

    add @notionhq/client package

---
+++ src/lib/notion.js
@@ -0,0 +1,27 @@
+import { Client } from "@notionhq/client";
+
+const notion = new Client({
+    auth: process.env.NOTION_API_KEY,
+});
+
+export const content_databaseId = process.env.SANGHAASPAT_CONTENT_DATABASE_ID;
+
+export const getDatabase = async (content_databaseId) => {
+    const response = await notion.databases.query({
+        database_id: content_databaseId,
+    });
+    return response.results;
+};
+
+export const getPage = async (pageId) => {
+    const response = await notion.pages.retrieve({ page_id: pageId });
+    return response;
+};
+
+export const getBlocks = async (blockId) => {
+    const response = await notion.blocks.children.list({
+        block_id: blockId,
+        page_size: 50,
+    });
+    return response;
+};
