cc204f936d9843cc4ede8b80dce2ebb5a1e56cf5
Evren Çağlayan <caglayanevren@gmail.com>
2022-06-14 17:30:56 +0300

    ELEVENTY_ENV to NODE_ENV

--- src/_11ty/helpers.js
+++ src/_11ty/helpers.js
@@ -23,128 +23,128 @@ const mdImageRegex = /^\!\[\]\(((?:\/|https?:\/\/)[\w\d./?=#]+)\)$/;
 
 // get cache contents from json file
 function readFromCache(cacheFilePath) {
-  if (fs.existsSync(cacheFilePath)) {
-    const cacheFile = fs.readFileSync(cacheFilePath);
-    return JSON.parse(cacheFile);
-  }
+    if (fs.existsSync(cacheFilePath)) {
+        const cacheFile = fs.readFileSync(cacheFilePath);
+        return JSON.parse(cacheFile);
+    }
 
-  // no cache found.
-  return {
-    lastFetched: null,
-  };
+    // no cache found.
+    return {
+        lastFetched: null,
+    };
 }
 
 // save combined webmentions in cache file
 function writeToCache(data, cacheFilePath, descriptor) {
-  // Don't actually write to cache on dev or cloudflare
-  if (process.env.ELEVENTY_ENV !== "devbuild") return;
+    // Don't actually write to cache on dev or cloudflare
+    if (process.env.NODE_ENV !== "devbuild") return;
 
-  const dir = "src/_cache";
-  const fileContent = JSON.stringify(data, null, 2);
-  // create cache folder if it doesnt exist already
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
-  }
-  // write data to cache json file
-  fs.writeFileSync(cacheFilePath, fileContent, (err) => {
-    if (err) throw err;
-    console.log(`>>> ${descriptor} cached to ${cacheFilePath}`);
-  });
+    const dir = "src/_cache";
+    const fileContent = JSON.stringify(data, null, 2);
+    // create cache folder if it doesnt exist already
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+    // write data to cache json file
+    fs.writeFileSync(cacheFilePath, fileContent, (err) => {
+        if (err) throw err;
+        console.log(`>>> ${descriptor} cached to ${cacheFilePath}`);
+    });
 }
 
 function hash(text) {
-  "use strict";
+    "use strict";
 
-  var hash = 5381,
-    index = text.length;
+    var hash = 5381,
+        index = text.length;
 
-  while (index) {
-    hash = (hash * 33) ^ text.charCodeAt(--index);
-  }
+    while (index) {
+        hash = (hash * 33) ^ text.charCodeAt(--index);
+    }
 
-  const hashNumber = hash >>> 0;
-  return hashNumber.toString();
+    const hashNumber = hash >>> 0;
+    return hashNumber.toString();
 }
 
 function shortHash(text) {
-  return hash(text).substring(0, 7);
+    return hash(text).substring(0, 7);
 }
 
 function getFileName(url) {
-  // get the filename from the path
-  const pathComponents = url.split("/");
+    // get the filename from the path
+    const pathComponents = url.split("/");
 
-  // break off cache busting string if there is one
-  let filename = pathComponents[pathComponents.length - 1].split("?");
-  return `${shortHash(url)}-${filename[0]}`;
+    // break off cache busting string if there is one
+    let filename = pathComponents[pathComponents.length - 1].split("?");
+    return `${shortHash(url)}-${filename[0]}`;
 }
 
 function getFolder(imgUrl, folder) {
-  if (imgUrl.includes('secure.notion-static.com')) return 'notion';
-  if (imgUrl.includes('photo.goodreads.com')) return 'goodreads';
+    if (imgUrl.includes("secure.notion-static.com")) return "notion";
+    if (imgUrl.includes("photo.goodreads.com")) return "goodreads";
 
-  return folder;
+    return folder;
 }
 
 function getLocalImageLink(imgUrl, fileName = "", folderParam = "ext") {
-  if (!imgUrl) return "";
+    if (!imgUrl) return "";
 
-  if (process.env.ELEVENTY_ENV !== "devbuild") return imgUrl;
+    if (process.env.NODE_ENV !== "devbuild") return imgUrl;
 
-  // skip local images, notion images
-  if (!external.test(imgUrl) || imgUrl.includes('secure.notion-static.com')) {
-    return imgUrl;
-  }
-
-  const folder = getFolder(imgUrl, folderParam);
-
-  const cache = readFromCache(IMG_CACHE_FILE_PATH);
-  if (cache[imgUrl]) {
-    const filePath = `./src${cache[imgUrl].url}`
-    if (fs.existsSync(filePath)) {
-      return cache[imgUrl].url;
+    // skip local images, notion images
+    if (!external.test(imgUrl) || imgUrl.includes("secure.notion-static.com")) {
+        return imgUrl;
     }
-    // it's probably downloading
-    return imgUrl;
-  }
 
-  const fn = fileName || getFileName(imgUrl);
-  const imagePath = `/img/${folder}/${fn}`;
-  const path = `./src${imagePath}`;
+    const folder = getFolder(imgUrl, folderParam);
 
-  if (!fs.existsSync(path)) {
-    fetch(imgUrl).then((res) => res.body.pipe(fs.createWriteStream(path)));
-    cache[imgUrl] = { url: imagePath };
-    writeToCache(cache, IMG_CACHE_FILE_PATH, "images");
-    // TODO: return local. For now, synce download is async, first run needs to use external url
-    return imgUrl;
-  } else {
-    console.error("> collision downloading image", imgUrl);
-  }
+    const cache = readFromCache(IMG_CACHE_FILE_PATH);
+    if (cache[imgUrl]) {
+        const filePath = `./src${cache[imgUrl].url}`;
+        if (fs.existsSync(filePath)) {
+            return cache[imgUrl].url;
+        }
+        // it's probably downloading
+        return imgUrl;
+    }
 
-  return imagePath;
+    const fn = fileName || getFileName(imgUrl);
+    const imagePath = `/img/${folder}/${fn}`;
+    const path = `./src${imagePath}`;
+
+    if (!fs.existsSync(path)) {
+        fetch(imgUrl).then((res) => res.body.pipe(fs.createWriteStream(path)));
+        cache[imgUrl] = { url: imagePath };
+        writeToCache(cache, IMG_CACHE_FILE_PATH, "images");
+        // TODO: return local. For now, synce download is async, first run needs to use external url
+        return imgUrl;
+    } else {
+        console.error("> collision downloading image", imgUrl);
+    }
+
+    return imagePath;
 }
 
 async function optimizeImage(src) {
-  if (!src) {
-    return src;
-  }
+    if (!src) {
+        return src;
+    }
 
-  const fileSource = src.startsWith("/img") ? `./src${src}` : src;
+    const fileSource = src.startsWith("/img") ? `./src${src}` : src;
 
-  let metadata = await Image(fileSource, {
-    widths: [1200],
-    outputDir: "_site/img",
-  });
+    let metadata = await Image(fileSource, {
+        widths: [1200],
+        outputDir: "_site/img",
+    });
 
-  let data = metadata.jpeg[metadata.jpeg.length - 1];
-  return data.url;
+    let data = metadata.jpeg[metadata.jpeg.length - 1];
+    return data.url;
 }
 
 module.exports = {
-  // replaceNotionMarkdown,
-  readFromCache,
-  writeToCache,
-  getLocalImageLink,
-  optimizeImage,
+    // replaceNotionMarkdown,
+    readFromCache,
+    writeToCache,
+    getLocalImageLink,
+    optimizeImage,
 };
--- src/_data/notion_posts.js
+++ src/_data/notion_posts.js
@@ -1,11 +1,7 @@
 const fetch = require("node-fetch");
 const fs = require("fs");
 const domain = require("./metadata.json").domain;
-const {
-  readFromCache,
-  writeToCache,
-  getLocalImageLink,
-} = require("../_11ty/helpers");
+const { readFromCache, writeToCache, getLocalImageLink } = require("../_11ty/helpers");
 const { fetchFromNotion, getNotionProps, getLocalImages, updateReddit, updateTweet } = require("../_11ty/notionHelpers");
 
 const { Client } = require("@notionhq/client");
@@ -21,110 +17,107 @@ const notion = new Client({ auth: TOKEN });
 const n2m = new NotionToMarkdown({ notionClient: notion });
 
 const getMetadata = (post) => {
-  return {
-    id: post.id,
-    created_time: post.created_time,
-    last_edited_time: post.last_edited_time,
-    cover: post.cover,
-    icon: post.icon,
-  };
+    return {
+        id: post.id,
+        created_time: post.created_time,
+        last_edited_time: post.last_edited_time,
+        cover: post.cover,
+        icon: post.icon,
+    };
 };
 
 async function fetchPage(pageId) {
-  const mdblocks = await n2m.pageToMarkdown(pageId);
-  // console.debug(`---------------`);
-  // console.debug(mdblocks);
+    const mdblocks = await n2m.pageToMarkdown(pageId);
+    // console.debug(`---------------`);
+    // console.debug(mdblocks);
 
-  const mdString = n2m.toMarkdownString(mdblocks);
-  // console.debug(`---------------`);
-  // console.debug(mdString);
-  // console.debug(`---------------`);
-  return mdString;
-  // return replaceNotionMarkdown(mdString);
+    const mdString = n2m.toMarkdownString(mdblocks);
+    // console.debug(`---------------`);
+    // console.debug(mdString);
+    // console.debug(`---------------`);
+    return mdString;
+    // return replaceNotionMarkdown(mdString);
 }
 
 // TODO: this won't delete posts deleted on notion
 async function fetchPosts(since) {
-  // If we dont have a domain name or token, abort
-  if (!DATABASE_ID || !TOKEN) {
-    console.warn(">>> unable to fetch posts: missing token or db id");
-    return null;
-  }
-
-  // const filters = since
-  //   ? {
-  //       property: "Last Edited",
-  //       date: { after: since },
-  //     }
-  //   : {};
-
-  const results = await fetchFromNotion(notion, DATABASE_ID, undefined);
-
-  if (results) {
-    const newPosts = {};
-    console.log(`>>> ${results.length} new posts fetched`);
-
-    for (const post of results) {
-      const content = await fetchPage(post.id);
-      const props = getNotionProps(post);
-      props['thumbnail'] = props['thumbnail'][0];//getLocalImages(post, 'Thumbnail', 'notion_post')[0];
-      newPosts[post.id] = {
-        ...getMetadata(post),
-        ...props,
-        content: content,
-      };
+    // If we dont have a domain name or token, abort
+    if (!DATABASE_ID || !TOKEN) {
+        console.warn(">>> unable to fetch posts: missing token or db id");
+        return null;
     }
 
-    return newPosts;
-  }
+    // const filters = since
+    //   ? {
+    //       property: "Last Edited",
+    //       date: { after: since },
+    //     }
+    //   : {};
 
-  return null;
+    const results = await fetchFromNotion(notion, DATABASE_ID, undefined);
+
+    if (results) {
+        const newPosts = {};
+        console.log(`>>> ${results.length} new posts fetched`);
+
+        for (const post of results) {
+            const content = await fetchPage(post.id);
+            const props = getNotionProps(post);
+            props["thumbnail"] = props["thumbnail"][0]; //getLocalImages(post, 'Thumbnail', 'notion_post')[0];
+            newPosts[post.id] = {
+                ...getMetadata(post),
+                ...props,
+                content: content,
+            };
+        }
+
+        return newPosts;
+    }
+
+    return null;
 }
 
 // Merge dicts
 function mergePosts(a = {}, b = {}) {
-  return { ...a, ...b };
+    return { ...a, ...b };
 }
-
 
 const todaysDate = new Date();
 function showPost(data) {
-  // FOR NOW: also filter posts without slug - don't want to have it change over time
-  const hasSlug = "slug" in data && data.slug !== '';
-  const isPublished = "published" in data && data.published === true;
-  const isFutureDate = !data.date_published || data.date_published > todaysDate;
-  return hasSlug && isPublished && !isFutureDate;
+    // FOR NOW: also filter posts without slug - don't want to have it change over time
+    const hasSlug = "slug" in data && data.slug !== "";
+    const isPublished = "published" in data && data.published === true;
+    const isFutureDate = !data.date_published || data.date_published > todaysDate;
+    return hasSlug && isPublished && !isFutureDate;
 }
 
 function checkDuplicates(posts) {
-  let seen = new Set();
-  var hasDuplicates = values.some(function(currentObject) {
-      return seen.size === seen.add(currentObject.name).size;
-  });
+    let seen = new Set();
+    var hasDuplicates = values.some(function (currentObject) {
+        return seen.size === seen.add(currentObject.name).size;
+    });
 }
 
 function filterDrafts(posts) {
-  const isDevEnv = process.env.ELEVENTY_ENV === "development";
+    const isDevEnv = process.env.NODE_ENV === "development";
 
-  if (isDevEnv) return Object.values(posts);
+    if (isDevEnv) return Object.values(posts);
 
-  return Object.values(posts).filter(post => showPost(post) === true);
+    return Object.values(posts).filter((post) => showPost(post) === true);
 }
-
 
 // TODO: Find duplicate slugs and error/warning
 
 module.exports = async function () {
-  console.log(">>> Checking for new posts...");
-  const posts = await fetchPosts();
+    console.log(">>> Checking for new posts...");
+    const posts = await fetchPosts();
 
-  await updateReddit(notion, posts, 'post');
-  await updateTweet(notion, posts, 'post');
-  const publishedPosts = filterDrafts(posts);
-  // updateReplyTo(notion, publishedPosts, 'post');
-  return publishedPosts;
+    await updateReddit(notion, posts, "post");
+    await updateTweet(notion, posts, "post");
+    const publishedPosts = filterDrafts(posts);
+    // updateReplyTo(notion, publishedPosts, 'post');
+    return publishedPosts;
 };
-
 
 // module.exports = async function () {
 //   console.log(">>> Reading posts from cache...");
@@ -137,7 +130,7 @@ module.exports = async function () {
 //   }
 
 //   // Only fetch new posts in production
-//   // if (process.env.ELEVENTY_ENV === "development") return filterDrafts(cache.posts);
+//   // if (process.env.NODE_ENV === "development") return filterDrafts(cache.posts);
 
 //   console.log(">>> Checking for new posts...");
 //   const newPosts = await fetchPosts(cache.lastFetched);
@@ -148,7 +141,7 @@ module.exports = async function () {
 //       posts: mergePosts(cache.posts, newPosts),
 //     };
 
-//     if (process.env.ELEVENTY_ENV === "devbuild") {
+//     if (process.env.NODE_ENV === "devbuild") {
 //       writeToCache(posts, CACHE_FILE_PATH, "posts");
 //     }
 
